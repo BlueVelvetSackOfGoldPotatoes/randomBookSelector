2c6fbf999366dcfc6ae01c9328f234fff5164254
GoHora de Carvalho <goncalo.horacarvalho@gmail.com>
2019-05-17 12:44:14 +0200

    First iteration of the program.

---
+++ bookSelector.py
@@ -0,0 +1,105 @@
+#!/usr/bin/env python
+"""
+SYNOPSIS
+
+    TODO random book gen [-h,--help] [-v,--verbose] [--version]
+
+DESCRIPTION
+
+    Picks a random book out of a list of books.
+
+EXAMPLES
+
+    TODO: Show some examples of how to use this script.
+
+EXIT STATUS
+
+    TODO: List exit codes
+
+AUTHOR
+
+    TODO: Hora de Carvalho <goncalo.horadecarvalho@gmail.com>
+
+LICENSE
+
+    This script is in the public domain, free from copyrights or restrictions.
+
+VERSION
+
+    $Id$
+"""
+
+import sys, os, traceback, optparse
+import time
+import re
+#from pexpect import run, spawn
+
+# generates a random number from 1 to s
+def randomSelector(s):
+    from random import randint
+    return randint(1, s)
+
+# checks if book file exists, if not creates it
+def checkFile():
+    try:      
+        # returns absolute path of file
+        path = os.path.abspath("randomBookSelector/bookList.txt")
+        # reads, line by line, strings in file and saves them to list 'lines'
+        with open(path) as f:
+            lines = f.read().splitlines()
+        return lines
+    except:
+        return -1
+
+def fillLibrary():
+    f= open("bookList.txt","w+")
+    library = input("How many books do you wish to add to the magical random library?")
+    for i in library:
+        f.write(input("Book " + i + ":\n"))
+
+def main ():
+    
+    global options, args
+
+    # try to open file it exist, if not create file and fill it
+    
+    lines = checkFile()
+    
+    if lines == -1 :
+        fillLibrary()
+    
+    lines = checkFile()    
+
+    key = input("pressing (0) will quit \npressing (1) will list the books in the library \npressing (2) will generate a book:\n")
+    if key == 0 :
+        sys.exit(0)
+    if key == 1 :
+        print(*lines, sep='\n')
+    if key == 2 :
+        n = randomSelector(len(lines))
+        print(lines[n])
+
+# came with the skeleton - unsure what it does
+if __name__ == '__main__':
+    try:
+        start_time = time.time()
+        parser = optparse.OptionParser(formatter=optparse.TitledHelpFormatter(), usage=globals()['__doc__'], version='$Id$')
+        parser.add_option ('-v', '--verbose', action='store_true', default=False, help='verbose output')
+        (options, args) = parser.parse_args()
+        #if len(args) < 1:
+        #    parser.error ('missing argument')
+        if options.verbose: print time.asctime()
+        main()
+        if options.verbose: print time.asctime()
+        if options.verbose: print 'TOTAL TIME IN MINUTES:',
+        if options.verbose: print (time.time() - start_time) / 60.0
+        sys.exit(0)
+    except KeyboardInterrupt, e: # Ctrl-C
+        raise e
+    except SystemExit, e: # sys.exit()
+        raise e
+    except Exception, e:
+        print 'ERROR, UNEXPECTED EXCEPTION'
+        print str(e)
+        traceback.print_exc()
+        os._exit(1)
